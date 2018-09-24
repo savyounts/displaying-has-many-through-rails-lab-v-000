a6b17419eb10511e24a685b0ca2774c64494d980
Savannah Younts <savyounts@gmail.com>
2018-09-24 19:45:58 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180924193647) do
+ActiveRecord::Schema.define(version: 20180924193825) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.string   "appointment_datetime"
+    t.integer  "paitent_id"
+    t.integer  "doctor_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
